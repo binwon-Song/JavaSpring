918614a876babad6b9b11bf7af1d0b4b609ba2f1
binwon-Song <binwon236@gmail.com>
2023-04-05 00:29:47 +0900

    signup and memberList

--- src/main/java/task/first/controller/MemberController.java
+++ src/main/java/task/first/controller/MemberController.java
@@ -3,7 +3,13 @@ package task.first.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import task.first.domain.Member;
 import task.first.serivce.MemberService;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -13,5 +19,25 @@ public class MemberController {
     @Autowired // 생성자 주입
     public MemberController(MemberService memberService) { //MemeberService를 인식 못함
         this.memberService = memberService;
+    }
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+
     }
 }
